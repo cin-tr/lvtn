9781bcd3bed3d054f7f7b58637d55c67197cb590
hoaitrinh <hoaitrinh2106@gmail.com>
2024-05-03 13:38:52 +0700

    done section infor doctor

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -1,5 +1,6 @@
 import { raw } from "body-parser";
 import db from "../models/index";
+import { where } from "sequelize";
 
 let getTopDoctorHome = (limitInput) => {
     return new Promise(async (resolve, reject) => {
@@ -63,22 +64,90 @@ let saveDetailInforDoctor = (inputData) => {
             if (
                 !inputData.doctorId ||
                 !inputData.contentHTML ||
-                !inputData.contentMarkdown
+                !inputData.contentMarkdown ||
+                !inputData.action
             ) {
                 resolve({
                     errCode: 1,
                     errMessage: "Missing Parameter!",
                 });
             } else {
-                await db.Markdown.create({
-                    contentHTML: inputData.contentHTML,
-                    contentMarkdown: inputData.contentMarkdown,
-                    description: inputData.description,
-                    doctorId: inputData.doctorId,
-                });
+                if (inputData.action === "CREATE") {
+                    await db.Markdown.create({
+                        contentHTML: inputData.contentHTML,
+                        contentMarkdown: inputData.contentMarkdown,
+                        description: inputData.description,
+                        doctorId: inputData.doctorId,
+                    });
+                } else if (inputData.action === "EDIT") {
+                    let doctorMarkdown = await db.Markdown.findOne({
+                        where: { doctorId: inputData.doctorId },
+                        raw: false,
+                    });
+
+                    if (doctorMarkdown) {
+                        doctorMarkdown.contentHTML = inputData.contentHTML;
+                        doctorMarkdown.contentMarkdown =
+                            inputData.contentMarkdown;
+                        doctorMarkdown.description = inputData.description;
+
+                        await doctorMarkdown.save();
+                    }
+                }
                 resolve({
                     errCode: 0,
                     errMessage: "Save infor doctor succeed!",
+                });
+            }
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+let getDetailDoctorById = (inputId) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!inputId) {
+                resolve({
+                    errCode: 1,
+                    errMessage: "Missing required parameter!",
+                });
+            } else {
+                let data = await db.User.findOne({
+                    where: {
+                        id: inputId,
+                    },
+                    attributes: {
+                        exclude: ["password"],
+                    },
+                    include: [
+                        {
+                            model: db.Markdown,
+                            attributes: [
+                                "description",
+                                "contentMarkdown",
+                                "contentHTML",
+                            ],
+                        },
+                        {
+                            model: db.Allcode,
+                            as: "positionData",
+                            attributes: ["valueEn", "valueVi"],
+                        },
+                    ],
+                    raw: false,
+                    nest: true,
+                });
+                if (data && data.image) {
+                    data.image = new Buffer(data.image, "base64").toString(
+                        "binary"
+                    );
+                }
+                if (!data) data = {};
+                resolve({
+                    errCode: 0,
+                    data: data,
                 });
             }
         } catch (e) {
@@ -91,4 +160,5 @@ module.exports = {
     getTopDoctorHome: getTopDoctorHome,
     getAllDoctors: getAllDoctors,
     saveDetailInforDoctor: saveDetailInforDoctor,
+    getDetailDoctorById: getDetailDoctorById,
 };
